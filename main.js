13b3344a415156a6d516d0a00707cc5fd0904938
Ethan Aldridge <ethanjaldridge97@gmail.com>
2023-12-05 11:06:38 -0500

    create workout advice generator

--- main.js
+++ main.js
@@ -9,3 +9,40 @@ Workout Advice Generator
 3. You should STOP: staying up late, eating junk food, skipping the gym
 */
 
+const workoutAdvice = {
+    goal: ['lose bodyfat', 'build muscle', 'gain strength'],
+    start: ['going to bed early', 'eating real food', 'training hard'],
+    stop: ['staying up late', 'eating junk food', 'skipping the gym']
+};
+
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num);
+};
+
+let yourAdvice = [];
+
+for (let advice in workoutAdvice) {
+    let selector = generateRandomNumber(workoutAdvice[advice].length)
+
+    switch (advice) {
+        case 'goal':
+            yourAdvice.push(`If your goal is to ${workoutAdvice[advice][selector]},`);
+            break;
+        case 'start':
+            yourAdvice.push(`you should start ${workoutAdvice[advice][selector]},`);
+            break;
+        case 'stop': 
+            yourAdvice.push(`and you should stop ${workoutAdvice[advice][selector]}.`);
+            break;
+        default:
+            yourAdvice.push('Invalid entry');
+            break;
+    };
+};
+
+function formatAdvice() {
+    const formattedAdvice = yourAdvice.join(' ');
+    console.log(formattedAdvice);
+};
+
+formatAdvice(yourAdvice);
