e028c6398292faf9763ccf8a7f7213ec165cc606
Ethan Aldridge <ethanjaldridge97@gmail.com>
2023-12-04 21:52:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+/* 
+Build message generator
+Everytime a user runs the program, they get a new, randomized output
+The output message should be made up of 3 different pieces of data 
+
+Workout Advice Generator
+1. Your GOAL is: lose bodyfat, build muscle, gain strength
+2. You should START: going to bed early, eating real food, training hard
+3. You should STOP: staying up late, eating junk food, skipping the gym
+*/
+
+console.log('I am a node program');
